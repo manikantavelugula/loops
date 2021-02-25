db47b9a35cab1df1793a7e12bbcef2b39143046e
manikantavelugula <79646882+manikantavelugula@users.noreply.github.com>
2021-02-25 19:33:10 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class loops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] cars= {"Tokoyo","mahindra","BMW","Ford","Skoda","Hundai"};
+		for (String i:cars) {
+			System.out.println(i);
+		}
+	}
+
+}
